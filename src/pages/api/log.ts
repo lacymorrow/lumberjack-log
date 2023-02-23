72602e596575b6bfe4e612b7f29f6050721951d3
Lacy Morrow <me@lacymorrow.com>
2023-02-23 15:10:03 -0500

    feat(log): adding logging; deploy to vercel

--- src/pages/api/log.ts
+++ src/pages/api/log.ts
@@ -11,11 +11,43 @@ export default function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
+
+	// // Block access to this route if no API key is provided
+	// if(!req.query.key) return res.status(404).json({ message: 'No API key provided.' });
+
+	// Block access to this route if the wrong API key is provided
+	if(process.env.LUMBERJACK_API_KEY && req.query.key !== process.env.LUMBERJACK_API_KEY) return res.status(404).json({ message: 'Wrong API key provided.' });
+
+	// Block access to this route if no message is provided
+	if(!req.query.message) return res.status(404).json({ message: 'No message provided.' });
+
+	// Create a new date object
+	const date = new Date();
+
 	try {
-		const { message } = req.query;
-		console.log(`Logged: ${message}`);
+		const { message, headers, referrer, reqPath  } = req.query;
+
+		// Log a separator
+		console.log();
+		console.log(`------------------`);
+
+		// Supply all available data
+		if (headers) console.log(date.toDateString(), `>>> Headers: ${headers}`);
+		if (referrer) console.log(date.toDateString(), `>>> Referrer: ${referrer}`);
+		if (reqPath) console.log(date.toDateString(), `>>> Path: ${reqPath}`);
+
+		// Log the date and message
+		console.log(date.toDateString())
+		console.log(`${message}`);
+
+		// Log a separator
+		console.log(`------------------`);
+		console.log();
+
+		// Return a success message
 		res.status(200).json({ message: 'Data received successfully' });
 	} catch (error) {
-		res.status(404).json({ message: 'Server error' });
+		console.log(error);
+		res.status(404).json({ message: `Error: ${error}` });
 	}
 }
